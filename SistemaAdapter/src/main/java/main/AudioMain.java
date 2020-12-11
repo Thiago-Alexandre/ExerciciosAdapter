e5adf8ebaf5bce25fe8530093e3614dc235dd23c
Thiago <thiago.alexandre.new@gmail.com>
2020-12-11 11:54:51 -0300

    Alterado sistema Adapter.

--- SistemaAdapter/src/main/java/main/AudioMain.java
+++ SistemaAdapter/src/main/java/main/AudioMain.java
@@ -3,40 +3,70 @@ package main;
 import model.AudioMp3;
 import model.AudioWav;
 import model.AudioWmaAdapter;
+import model.FormatoAudio;
 import model.Wma;
 
-public class Main {
+public class AudioMain {
+    
+    public static FormatoAudio audio;
     
     public static void main(String[] args) {
         
         System.out.println("************************************************************");
         System.out.println("Iniciando o Sistema Adapter com base no problema dos formatos de Áudio...");
-        Player player = new Player();
         System.out.println("************************************************************");
         System.out.println("Testando formato MP3...");
-        player.setAudio(new AudioMp3());
-        player.testeAudio("testeMP3");
+        audio = new AudioMp3();
+        testeAudio("testeMP3");
         System.out.println("************************************************************");
         System.out.println("Testando formato WAV...");
-        player.setAudio(new AudioWav());
-        player.testeAudio("testeWAV");
+        audio = new AudioWav();
+        testeAudio("testeWAV");
         System.out.println("************************************************************");
         System.out.println("Testando formato WMA...");
         Wma wma = new Wma();
-        player.setAudio(new AudioWmaAdapter(wma));
-        player.testeAudio("testeWMA");
+        audio = new AudioWmaAdapter(wma);
+        testeAudio("testeWMA");
+        System.out.println("************************************************************");
+        System.out.println("Iniciando o Sistema Adapter com exceções...");
         System.out.println("************************************************************");
         System.out.println("Testando formato MP3...");
-        player.setAudio(new AudioMp3());
-        player.testeExcecao("testeMP3");
+        audio = new AudioMp3();
+        testeExcecao("testeMP3");
         System.out.println("************************************************************");
         System.out.println("Testando formato WAV...");
-        player.setAudio(new AudioWav());
-        player.testeExcecao("testeWAV");
+        audio = new AudioWav();
+        testeExcecao("testeWAV");
         System.out.println("************************************************************");
         System.out.println("Testando formato WMA...");
         wma = new Wma();
-        player.setAudio(new AudioWmaAdapter(wma));
-        player.testeExcecao("testeWMA");
+        audio = new AudioWmaAdapter(wma);
+        testeExcecao("testeWMA");
+    }
+    
+    public static void testeAudio(String arquivo){
+    
+        System.out.println("Abrindo arquivo...");
+        audio.abrir(arquivo);
+        System.out.println("------------------------------");
+        System.out.println("Iniciando reprodução do arquivo...");
+        audio.reproduzir();
+        System.out.println("------------------------------");
+        audio.parar();
+        System.out.println("------------------------------");
+    }
+    
+    public static void testeExcecao(String arquivo){
+        
+        audio.parar();
+        System.out.println("------------------------------");
+        audio.reproduzir();
+        System.out.println("------------------------------");
+        audio.parar();
+        System.out.println("------------------------------");
+        audio.abrir(arquivo);
+        System.out.println("------------------------------");
+        audio.parar();
+        System.out.println("------------------------------");
     }
 }
